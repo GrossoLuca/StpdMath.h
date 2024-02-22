b7d304eb2a0df047381f9259d3c1889dabaf47fc
Luca Grosso <grossolu@icloud.com>
2024-02-22 09:37:35 +0100

    Added gitignore and mean function

---
+++ stpdMath.c
@@ -0,0 +1,16 @@
+/**
+ * @file stpdMath.c
+ * @author Luca Grosso (grossolu@icloud.com)
+ * @brief Include some stupidy functions
+ * @version 0.1
+ * @date 2024-02-22
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include "stpdMath.h"
+
+float mean(int a, int b)
+{
+    return ((float)(a+b)/2.0);
+}
---
+++ stpdMath.h
@@ -0,0 +1,18 @@
+/**
+ * @file stpdmath.h
+ * @author Luca Grosso (grossolu@icloud.com)
+ * @brief Little stupidy library about math
+ * @version 0.1
+ * @date 2024-02-22
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef _STPDMATHH_
+#define _STPDMATHH_
+
+float mean (int a, int b);
+
+
+#endif
